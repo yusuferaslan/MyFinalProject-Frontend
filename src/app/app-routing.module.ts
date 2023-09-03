612fe1c29c0c9e34f67fa935ecf7f145f79b241d
yusuf <yusuferaslan@hotmail.com>
2023-09-04 02:20:24 +0300

    Routing Module,Actived Route yapısı, Category Component, Category Service yapısı, Category'e göre ürün listeleme, Bootstrap, Jquery paketleri

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: ProductComponent }, //Anasayfada (http://localhost:4200/) gösterilecek komponent
+  { path: 'products', component: ProductComponent }, //Anasayfa sonuna products eklerse (http://localhost:4200/products) gösterilecek komponent
+  { path: "products/category/:categoryId", component: ProductComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,17 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ProductResponseModel } from '../models/productResponseModel';
 import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  apiUrl = 'https://localhost:44353/api/Products/getall';
+  apiUrl = 'https://localhost:44353/api/';
 
   constructor(private httpClient: HttpClient) {}
 
-  getProducts(): Observable<ProductResponseModel> {
-    return this.httpClient.get<ProductResponseModel>(this.apiUrl);
+  getProducts(): Observable<ListResponseModel<Product>> {
+    let newPath= this.apiUrl+"Products/getall"
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
+
+  getProductsByCategory(categoryId:number): Observable<ListResponseModel<Product>> {
+    let newPath=this.apiUrl+"Products/getbycategory?categoryId="+categoryId
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+
 }
